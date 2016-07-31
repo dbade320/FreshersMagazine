86df439429136d4cbfd2c78a305a53a2ee12b17d
root <root@ip-172-31-16-95.ap-south-1.compute.internal>
2016-07-31 07:26:50 +0000

    database updated

--- FreshersMagazine/wp-config-sample.php
+++ FreshersMagazine/wp-config-sample.php
@@ -29,7 +29,7 @@ define('DB_USER', 'freshersmagazine');
 define('DB_PASSWORD', 'freshersmagazine');
 
 /** MySQL hostname */
-define('DB_HOST', 'freshersmagazine.ct3aqqij2ijm.ap-south-1.rds.amazonaws.com');
+define('DB_HOST', 'freshersmagazine.ct3aqqij2ijm.ap-south-1.rds.amazonaws.com:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
